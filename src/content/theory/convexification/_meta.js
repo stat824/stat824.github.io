2c53dd18d533f4f8a4a0db5a6311da9aa9464c76
jlxh824 <48860678+jlxh824@users.noreply.github.com>
2025-05-27 00:19:54 +1000

    .

--- src/content/theory/convexification/_meta.js
+++ src/content/theory/convexification/_meta.js
@@ -1,6 +1,6 @@
 export default {
   principle: 'The convexification principle',
-  zhang: 'Zhang's lemma',
+  zhang: 'Zhang\'s lemma',
   oracle: 'Oracle inequalities for the $\phi$ risk',
   svm: 'Support vector machines'
 }
